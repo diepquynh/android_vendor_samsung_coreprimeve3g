d1e2e16b9674cd1a64c065ad97ce8ed159a35663
Remilia Scarlet <remilia.1505@gmail.com>
2017-10-04 11:30:14 +0000

    blobs: Import stock prebuilt gralloc
    Change-Id: I6a6a5d782daed499e511aedcfafd61a0d5152fab
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- coreprimeve3g-vendor-blobs.mk
+++ coreprimeve3g-vendor-blobs.mk
@@ -18,6 +18,7 @@ PROPRIETARY_FILES := \
     lib/libisp.so \
     lib/libface_finder.so \
     lib/hw/camera.sc8830.so \
+    lib/hw/gralloc.sc8830.so \
     lib/hw/sensors.sc8830.so
 
 PRODUCT_COPY_FILES += \
